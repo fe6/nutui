598993b2f174bfbf8c7dbd691ab951e62fcbf497
李梦龙 <649076408@qq.com>
2022-10-15 17:28:09 +0800

    fix(build): h5 error

--- vite.config.build.ts
+++ vite.config.build.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     minify: false,
     rollupOptions: {
       // 请确保外部化那些你的库中不需要的依赖
-      external: ['vue', 'vue-router'],
+      external: ['vue', 'vue-router', '@tarojs/taro'],
       output: {
         banner,
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
