dafb6ac98ac291fd418dbe2ec4d59c9938328a30
李梦龙 <649076408@qq.com>
2022-11-24 15:32:37 +0800

    fix(infiniteloading): h5 error

--- src/packages/__VUE/infiniteloading/index.taro.vue
+++ src/packages/__VUE/infiniteloading/index.taro.vue
@@ -4,6 +4,7 @@
     scrollY="true"
     style="height: 100%"
     id="scroller"
+    ref="scroller"
     @scrolltolower="lower"
     @scroll="scroll"
     @touchstart="touchStart"
@@ -35,7 +36,17 @@
   </scroll-view>
 </template>
 <script lang="ts">
-import { toRefs, onMounted, reactive, computed, CSSProperties } from 'vue';
+import {
+  toRefs,
+  onMounted,
+  reactive,
+  computed,
+  CSSProperties,
+  onUnmounted,
+  ref,
+  onActivated,
+  onDeactivated
+} from 'vue';
 import { createComponent } from '@/packages/utils/create';
 const { componentName, create, translate } = createComponent('infiniteloading');
 import Icon from '../icon/index.taro.vue';
@@ -105,7 +116,11 @@ export default create({
       refreshMaxH: 0,
       y: 0,
       x: 0,
-      distance: 0
+      distance: 0,
+      scrollEl: window as Window | HTMLElement | (Node & ParentNode),
+      scroller: null as null | HTMLElement,
+      refreshTop: null as null | HTMLElement,
+      beforeScrollTop: 0
     });
 
     const classes = computed(() => {
@@ -203,11 +218,109 @@ export default create({
       state.isTouching = false;
     };
 
+    const getWindowScrollTop = () => {
+      return window.pageYOffset !== undefined
+        ? window.pageYOffset
+        : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    };
+
+    const calculateTopPosition = (el: HTMLElement): number => {
+      return !el ? 0 : el.offsetTop + calculateTopPosition(el.offsetParent as HTMLElement);
+    };
+
+    const isScrollAtBottom = () => {
+      let offsetDistance = 0;
+      let resScrollTop = 0;
+      let direction = 'down';
+      const windowScrollTop = getWindowScrollTop();
+      if (props.useWindow) {
+        if (state.scroller) {
+          offsetDistance =
+            calculateTopPosition(state.scroller) + state.scroller.offsetHeight - windowScrollTop - window.innerHeight;
+        }
+        resScrollTop = windowScrollTop;
+      } else {
+        const { scrollHeight, clientHeight, scrollTop } = state.scrollEl as HTMLElement;
+
+        offsetDistance = scrollHeight - clientHeight - scrollTop;
+        resScrollTop = scrollTop;
+      }
+
+      if (state.beforeScrollTop > resScrollTop) {
+        direction = 'up';
+      } else {
+        direction = 'down';
+      }
+
+      state.beforeScrollTop = resScrollTop;
+
+      emit('scroll-change', resScrollTop);
+
+      return offsetDistance <= props.threshold && direction == 'down';
+    };
+
+    const requestAniFrame = () => {
+      return (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        function (callback) {
+          window.setTimeout(callback, 1000 / 60);
+        }
+      );
+    };
+
+    const handleScroll = () => {
+      requestAniFrame()(() => {
+        if (!isScrollAtBottom() || !props.hasMore || state.isInfiniting) {
+          return false;
+        } else {
+          state.isInfiniting = true;
+          emit('load-more', infiniteDone);
+        }
+      });
+    };
+
+    const scrollListener = () => {
+      state.scrollEl.addEventListener('scroll', handleScroll, props.useCapture);
+    };
+
+    const getParentElementH5 = (el: HTMLElement) => {
+      return !!props.containerId ? document.querySelector(`#${props.containerId}`) : el && el.parentNode;
+    };
+
     onMounted(() => {
-      state.refreshMaxH = props.upperThreshold;
-      setTimeout(() => {
-        getScrollHeight();
-      }, 200);
+      if (Taro.getEnv() === 'WEB') {
+        const parentElement = getParentElementH5(state.scroller as HTMLElement) as Node & ParentNode;
+        state.scrollEl = props.useWindow ? window : parentElement;
+        scrollListener();
+      } else {
+        state.refreshMaxH = props.upperThreshold;
+        setTimeout(() => {
+          getScrollHeight();
+        }, 200);
+      }
+    });
+
+    onUnmounted(() => {
+      if (Taro.getEnv() === 'WEB') {
+        state.scrollEl.removeEventListener('scroll', handleScroll, props.useCapture);
+      }
+    });
+
+    const isKeepAlive = ref(false);
+
+    onActivated(() => {
+      if (Taro.getEnv() === 'WEB' && isKeepAlive.value) {
+        isKeepAlive.value = false;
+        scrollListener();
+      }
+    });
+
+    onDeactivated(() => {
+      if (Taro.getEnv() === 'WEB') {
+        isKeepAlive.value = true;
+        state.scrollEl.removeEventListener('scroll', handleScroll, props.useCapture);
+      }
     });
 
     return {
